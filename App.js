3b4df63e14ad688acd35d1e66e5450375cfd098a
Hakan Özbeyen <53910749+Hknzbyn@users.noreply.github.com>
2022-01-06 18:05:50 +0300

    initial

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text,Dimensions,StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import News from './src/screens/News';
+import Listnews from './src/components/Listnews'
+
+const { width, height } = Dimensions.get('window');
+
+
+export default function App() {
+  return (
+
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+       <Listnews/>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width:width,
+    height:height,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
